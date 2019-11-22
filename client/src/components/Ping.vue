007a79f83af6203d375a0ff82b4ad16b8e277920
Gabriel Oest <bieloest@gmail.com>
2019-11-22 15:44:43 +0000

    Add Vue client and Python server

---
+++ client/src/components/Ping.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <button type="button" class="btn btn-primary">{{ msg }}</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Ping',
+  data() {
+    return {
+      msg: '',
+    };
+  },
+  methods: {
+    getMessage() {
+      const path = 'https://5000-e119fca1-40fb-4dca-bd7e-ca92045e6381.ws-us02.gitpod.io/ping';
+      axios.get(path)
+        .then((res) => {
+          this.msg = res.data;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    },
+  },
+  created() {
+    this.getMessage();
+  },
+};
+</script>
